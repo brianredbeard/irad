b92d65041767ea716a44ca69a251196ae0bff1cc
Brian 'redbeard' Harrington <redbeard@dead-city.org>
2024-05-20 22:45:03 -0700

    wifi credentials are provided as env vars
    This commit updates the structure of everything to accept the following
    environment variables to configure the wifi:
    
      - WIFI_SSID
      - WIFI_PASSWORD
    
    While it's bad practice, if you won't be pushing changes back upstream
    you can also edit `platformio.ini`.

--- src/main.ino
+++ src/main.ino
@@ -1,13 +1,21 @@
 #include "Arduino.h"
 #include "WiFi.h"
 #include "Audio.h"
+
 #define SD_CS     4
 #define I2S_DOUT  21
 #define I2S_BCLK  17
 #define I2S_LRC   18
+
+#ifndef WIFI_SSID
+  #error "WIFI_SSID is not defined"
+#endif
+
+#ifndef WIFI_PASSWORD
+  #error "WIFI_PASSWORD is not defined"
+#endif
+
 Audio audio;
-String ssid =     "";
-String password = "";
 
 void setup() {
   pinMode(SD_CS, OUTPUT);
@@ -15,47 +23,47 @@ void setup() {
   Serial.begin(115200);
   WiFi.disconnect();
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid.c_str(), password.c_str());
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED)
-  delay(1500);
+    delay(1500);
   audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
   audio.setVolume(50);
   audio.connecttohost("https://somafm.com/live.pls");
 }
 
-void loop()
-{
+void loop() {
   audio.loop();
 }
 
 // optional
-void audio_info(const char *info){
-    Serial.print("info        "); Serial.println(info);
+void audio_info(const char *info) {
+  Serial.print("info        "); Serial.println(info);
 }
-void audio_id3data(const char *info){  //id3 metadata
-    Serial.print("id3data     ");Serial.println(info);
+void audio_id3data(const char *info) {  //id3 metadata
+  Serial.print("id3data     "); Serial.println(info);
 }
-void audio_eof_mp3(const char *info){  //end of file
-    Serial.print("eof_mp3     ");Serial.println(info);
+void audio_eof_mp3(const char *info) {  //end of file
+  Serial.print("eof_mp3     "); Serial.println(info);
 }
-void audio_showstation(const char *info){
-    Serial.print("station     ");Serial.println(info);
+void audio_showstation(const char *info) {
+  Serial.print("station     "); Serial.println(info);
 }
-void audio_showstreamtitle(const char *info){
-    Serial.print("streamtitle ");Serial.println(info);
+void audio_showstreamtitle(const char *info) {
+  Serial.print("streamtitle "); Serial.println(info);
 }
-void audio_bitrate(const char *info){
-    Serial.print("bitrate     ");Serial.println(info);
+void audio_bitrate(const char *info) {
+  Serial.print("bitrate     "); Serial.println(info);
 }
-void audio_commercial(const char *info){  //duration in sec
-    Serial.print("commercial  ");Serial.println(info);
+void audio_commercial(const char *info) {  //duration in sec
+  Serial.print("commercial  "); Serial.println(info);
 }
-void audio_icyurl(const char *info){  //homepage
-    Serial.print("icyurl      ");Serial.println(info);
+void audio_icyurl(const char *info) {  //homepage
+  Serial.print("icyurl      "); Serial.println(info);
 }
-void audio_lasthost(const char *info){  //stream URL played
-    Serial.print("lasthost    ");Serial.println(info);
+void audio_lasthost(const char *info) {  //stream URL played
+  Serial.print("lasthost    "); Serial.println(info);
 }
-void audio_eof_speech(const char *info){
-    Serial.print("eof_speech  ");Serial.println(info);
+void audio_eof_speech(const char *info) {
+  Serial.print("eof_speech  "); Serial.println(info);
 }
+
